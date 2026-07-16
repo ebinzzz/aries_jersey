d0c54dd7f2bad00b75e467147783fbe51349c57b
ebinzzz <ebinbenny777@gmail.com>
2026-07-16 17:33:33 +0530

    added navigation link from admin to landing page

--- admin/_partials/sidebar.php
+++ admin/_partials/sidebar.php
@@ -39,6 +39,12 @@ $admin_username = $_SESSION['admin_username'] ?? 'Admin';
                 <span>Migration Tracker</span>
             </a>
         </li>
+        <li class="sidebar-menu-item">
+            <a href="../" target="_blank">
+                <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+                <span>View Public Site</span>
+            </a>
+        </li>
     </ul>
     
     <div class="sidebar-footer">
